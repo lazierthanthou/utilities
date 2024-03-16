08ef7626486753fd831363d87fdc13385ac0cb00
lazierthanthou <mrinal.kant@gmail.com>
2024-03-16 09:29:30 +0530

    Add files via upload

---
+++ github_reader.py
@@ -0,0 +1,29 @@
+import requests, base64
+import json
+
+class Github_Reader():
+	def __init__(self, owner, repo, token):
+		self.owner = owner
+		self.repo = repo
+		self._headers = {
+			"Authorization": f'Bearer {token}',
+			"Accept": "application/vnd.github.v3+json"
+		}
+		return
+
+	def set_repo(self, owner, repo):
+		self.owner = owner
+		self.repo = repo
+		return
+
+	def get_file(self, file_path):
+		url = f'https://api.github.com/repos/{self.owner}/{self.repo}/contents/{file_path}'
+		response = requests.get(url, headers=self._headers)
+
+		if response.status_code == 200:
+			file_content = response.json()['content']
+			file_content = base64.b64decode(file_content).decode('utf-8')
+			return file_content
+		else:
+			print("Error:", response.status_code)
+			return ''
